ce1cb08564a3dcd7a7591659b203587d20bc7eb4
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-15 18:50:26 +0100

    adds array

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const teamMembers = [
+  
+    {
+        name:"Wayne",
+        surname:"Barnett",
+        profession:"Founder & CEO"
+        
+    },
+    {
+        name:"Angela",
+        surname:"Caroll",
+        profession:"Chief Editor"
+        
+    },
+    {
+        name:"Walter",
+        surname:"Gordon",
+        profession:"Office Manager	 "
+        
+    },
+    {
+        name:"Angela",
+        surname:"Lopez",
+        profession:"Social Media Manager"
+        
+    },
+    {
+        name:"Scott",
+        surname:"Estrada",
+        profession:"Developer"
+       
+    },
+    {
+        name:"Barbara",
+        surname:"Ramos",
+        profession:"Graphic Designer"
+       
+    }
+    
+]
